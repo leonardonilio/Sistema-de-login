b9139a22a2b7cc407b272cd3a6e8e26e9a205af6
leonardonilio <leonardoniliodossantos08@gmail.com>
2026-02-02 17:26:40 -0300

    Melhora do visual

--- user_page.php
+++ user_page.php
@@ -16,11 +16,20 @@ if (!isset($_SESSION['email'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="style.css">
 </head>
-<body style="background: #fff;">
+<body style="background: linear-gradient(135deg, #0f0f1e 0%, #1f1f3a 100%);">
+         <main class="container">        
     <div class="box">
         <h1>Bem-vindo, <span><?= $_SESSION['name']; ?></span></h1>
         <p>Essa é a pagina de <span>Usuário</span></p>
         <button onclick="window.location.href='logout.php'">Sair</button>
+        <div class="tech-circle">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+        </div>
+    
     </div>
+        </main>
 </body>
 </html>
